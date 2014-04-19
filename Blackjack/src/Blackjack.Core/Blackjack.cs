d1b85f5c2d517dab3e08fa2b1a1f217162c9a4c5
DavidEvans66 <david.evans@neuri.co.uk>
2014-04-19 12:26:44 +0100

    Test Fixture wired up for PlayerWinsOrLoses
    Implementation (of Result method) stubbed to fail

--- Blackjack/src/Blackjack.Core/Blackjack.cs
+++ Blackjack/src/Blackjack.Core/Blackjack.cs
@@ -4,6 +4,9 @@ namespace Blackjack.Core
 {
 	public class BlackjackApplication
 	{
+		public string Result(string PlayerCard, string DealerCard) {
+			return "Result method not implemented";
+		}
 	}
 
 }
---
+++ Blackjack/src/Blackjack.FitFixtures/PlayerOutcomeFixture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using fit;
+using Blackjack.Core;
+
+namespace Blackjack.FitFixtures
+{
+    public class PlayerOutcomeFixture: ColumnFixture{
+        public string PlayerCard, DealerCard;
+        public string PlayerOutcome() {
+        	BlackjackApplication game = new BlackjackApplication();
+        	return game.Result(PlayerCard, DealerCard);
+        }
+    }
+}
+
